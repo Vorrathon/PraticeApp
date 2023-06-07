d9088118dd7efae143bc2ae28ef01c7f2b785edd
Vorrathon <neofreedom_2539@gmail.com>
2023-06-07 21:12:38 +0700

    finish fragment Controller New

---
+++ app/src/main/java/com/patice/layoutactivtyfragment/fragments/ThridFragment.kt
@@ -0,0 +1,28 @@
+package com.patice.layoutactivtyfragment.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.patice.layoutactivtyfragment.R
+
+class ThridFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_thrid, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnNext = view.findViewById<Button>(R.id.btnNext)
+        btnNext.setOnClickListener {
+            findNavController().navigate(R.id.firstFragment)
+        }
+    }
+}
